bfef8045d8b153acc0c25c853696529fef81ce5b
Real Ocee <onasosi@gmail.com>
2022-08-25 14:09:48 +0100

    added

--- Colour Flipper(1P)/app.js
+++ Colour Flipper(1P)/app.js
@@ -4,7 +4,13 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
     // get random number between 0 - 3
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber() {
+    return Math.random();
+}
