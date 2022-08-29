08d4dd1b16fb5b783e27dfd313799a62ca601c35
Real Ocee <onasosi@gmail.com>
2022-08-29 14:05:49 +0100

    added

--- Counter(2P)/app.js
+++ Counter(2P)/app.js
@@ -1,29 +1,29 @@
 // set initial count
 let count = 0;
 
-// select value and buttons
+// select value and buttons 
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
-btns.forEach(function (btn){
-    btn.addEventListener("clicks", function (e) {
-        const style = e.currentTarget.classList;
-        if (StyleSheet.contains("decrease")) {
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
             count--;
-        } else if (style.contains("increase")) {
+        } else if (styles.contains("increase")) {
             count++;
         } else {
             count = 0;
         }
-        if(count > 0){
+        if (count > 0) {
             value.style.color = "green";
         }
-        if (count < 0){
-            value.style.color = 'red'
+        if (count < 0) {
+            value.style.color = "red";
         }
-        if(count === 0){
-            value.style.color = "#222"
+        if (count === 0){
+            value.style.color = "#222";
         }
         value.textContent = count;
-    });
+    })
 });
